20610066600513adbacd3eca225d056a1b614aef
Richard So <razs.apps@gmail.com>
2014-07-03 01:38:11 +0000

    Fixed issue with image paths that occur to usemin in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -228,8 +228,14 @@ module.exports = function (grunt) {
     usemin: {
       html: ['<%= yeoman.dist %>/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      js: '<%= yeoman.dist %>/scripts/{,*/}*.js',
       options: {
-        assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images']
+        assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images'],
+        patterns: {
+            js: [
+                [/(images\/.*?\.(?:gif|jpeg|jpg|png|webp))/gm, 'Update the JS for image paths']
+            ]
+        }
       }
     },
 
