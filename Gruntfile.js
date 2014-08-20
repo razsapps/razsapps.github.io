4ffa7e79683806f1f20c0afca230f52a554931bd
Richard So <razs.apps@gmail.com>
2014-08-20 21:25:23 +0000

    Added the ability to view the site externally after laucnhing grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,9 +88,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: grunt.option('port') || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: grunt.option('host') || 'localhost',
         livereload: 35729
       },
       livereload: {
@@ -110,7 +110,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: grunt.option('port') || 9001,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
